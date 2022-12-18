5cfb1b38e4bafbd3b89367fee150e057297c2590
Mateus Oliveira de Almeida <mateusalmeida0070@gmail.com>
2022-12-18 02:20:34 -0300

    Commit 18/12/2022

--- trash/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('USUARIO', 'kempe714_tecno');
-define('SENHA', '1rdU%LQyG{Xs');
-define('DB', 'kempe714_tecnologia');
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
